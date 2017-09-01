05bd360c9b10c6f56d96af3eda49a63e5f4be246
TONYSUKANDAR <SUKANDAR@MAIL.com>
2017-09-01 16:40:16 +0200

    Create show and new dinosaur pages

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -4,7 +4,7 @@ class DinosaursController < ApplicationController
   end
 
   def show
-    @dinosaur = Dinosaur.find(params[:id])
+    @dinosaur = dinosaur_find
   end
 
   def new
@@ -12,15 +12,41 @@ class DinosaursController < ApplicationController
   end
 
   def create
-    dinosaur_params = params.require(:dinosaur).permit(:name, :age, :image_url)
-
     @dinosaur = Dinosaur.new(dinosaur_params)
-
     if @dinosaur.save
        redirect_to @dinosaur
     else
        render 'new'
     end
+  end
+
+  def edit
+    @dinosaur = dinosaur_find
+  end
+
+  def update
+    @dinosaur = dinosaur_find
+    if @dinosaur.update_attributes(dinosaur_params)
+      redirect_to @dinosaur
+    else
+      render 'edit'
+    end
+  end
+
+    def destroy
+      @dinosaur = dinosaur_find
+      @dinosaur.destroy
+      redirect_to dinosaurs_path
+    end
+
+private
+
+  def dinosaur_params
+    params.require(:dinosaur).permit(:name, :age, :image_url)
+  end
+
+  def dinosaur_find
+    Dinosaur.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
   root "dinosaurs#index"
 
   get 'dinosaurs' => 'dinosaurs#index'
-  get 'dinosaurs/new' => "dinosaurs#new", as: :new_dinosaur
-  get 'dinosaurs/:id' => 'dinosaurs#show', as: :dinosaur
-
   post "dinosaurs" => "dinosaurs#create"
 
+  get 'dinosaurs/new' => "dinosaurs#new", as: :new_dinosaur
+  get 'dinosaurs/:id' => 'dinosaurs#show', as: :dinosaur
+  get 'dinosaurs/:id/edit' => 'dinosaurs#edit', as: :edit_dinosaur
+  patch "dinosaurs/:id" => "dinosaurs#update"
+
+  delete "dinosaurs/:id" => "dinosaurs#destroy"
 end
