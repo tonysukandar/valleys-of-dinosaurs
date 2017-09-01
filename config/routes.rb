6edea5093668397fe95757adea6beb1ffe8210a1
TONYSUKANDAR <SUKANDAR@MAIL.com>
2017-09-01 15:12:46 +0200

    Create show and new dinosaur pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "dinosaurs#index"
 
-  get "dinosaurs" => "dinosaurs#index"
+  get 'dinosaurs' => 'dinosaurs#index'
+  get 'dinosaurs/new' => "dinosaurs#new", as: :new_dinosaur
+  get 'dinosaurs/:id' => 'dinosaurs#show', as: :dinosaur
+
+  post "dinosaurs" => "dinosaurs#create"
+
 end
