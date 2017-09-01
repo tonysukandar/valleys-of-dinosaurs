98f3694f8fcd28249fc197dbf7a542b57db21f88
TONYSUKANDAR <SUKANDAR@MAIL.com>
2017-09-01 13:04:33 +0200

    Create dinosaur model

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,14 @@
 dino1 = Dinosaur.create!( { name: "Iguanodon", age: 3, image_url: "https://goo.gl/wB6q9q" } )
 dino2 = Dinosaur.create!( { name: "Mosasaurus", age: 10, image_url: "https://goo.gl/aoZsYt" } )
 dino3 = Dinosaur.create!( { name: "Hungarosaurus", age: 15, image_url: "https://goo.gl/EqRlgP" } )
+
+def random
+  @random_age = rand(0..100)
+end
+
+random
+dino4 = Dinosaur.create!( { name: "Stegosaurus", age: "#{@random_age}", image_url: "https://wikikids.nl/Bestand:Stegosaurus.jpg" } )
+random
+dino5 = Dinosaur.create!( { name: "Triceratops", age: "#{@random_age}", image_url: "https://wikikids.nl/Bestand:Triceratops_BW.jpg" } )
+random
+dino6 = Dinosaur.create!( { name: "Torosaurus", age: "#{@random_age}", image_url: "https://wikikids.nl/Bestand:Torosaurus.jpg" } )
